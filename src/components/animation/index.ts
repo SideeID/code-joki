c3c2b25dc859f0ea0c2ec57e833cdebc6e48ea52
SideeID <sandipratama164026@gmail.com>
2025-05-02 14:37:18 +0700

    feat: create animation index file to export animation components

---
+++ src/components/animation/index.ts
@@ -0,0 +1,6 @@
+export { BlurFade } from './blur-fade';
+export { TextAnimate } from './text-animate';
+export { CountUp } from './count-up';
+export * from './marquee';
+export * from './number-ticker';
+export * from './border-beam';

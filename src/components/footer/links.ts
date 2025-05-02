728cc5f597d4614be46dff83f983a19740f6a7c2
SideeID <sandipratama164026@gmail.com>
2025-05-02 14:38:05 +0700

    feat: add links data for footer navigation

---
+++ src/components/footer/links.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    label: "Beranda",
+    href: "/",
+  },
+  {
+    label: "Tentang kami",
+    href: "/tentang-kami",
+  },
+  {
+    label: "Promo",
+    href: "/acara",
+  },
+  {
+    label: "Layanan",
+    href: "/merchandise",
+  },
+];

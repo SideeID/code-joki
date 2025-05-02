ea978bc57fd434d752b44d91d32ee89bddebafba
SideeID <sandipratama164026@gmail.com>
2025-05-02 14:40:00 +0700

    feat: create home index file to export main components

---
+++ src/components/page/home/index.ts
@@ -0,0 +1,5 @@
+export { Hero } from './hero';
+export { About } from './about';
+export { MarqueeMember } from './marquee-member';
+export { CallToActionJoin } from './cta-join';
+export { ReadyProjects } from './ready-projects';

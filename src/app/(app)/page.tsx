eb39b20d7145ffbe008bd4330fb43c56b2ac0648
SideeID <sandipratama164026@gmail.com>
2025-05-02 14:36:47 +0700

    feat: implement Home component with layout and sections

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,33 @@
+import {
+  Hero,
+  About,
+  MarqueeMember,
+  CallToActionJoin,
+  ReadyProjects,
+} from '@/components/page/home';
+import { Shade } from '@/components/ui/shade';
+
+export default function Home() {
+  return (
+    <>
+      <Shade
+        corners={['top-left', 'top-right']}
+        cornerClass='border-foreground/75 border-t-0 h-12'
+        className="h-14 after:content-[''] after:w-full after:h-14 after:bg-gradient-to-t after:from-background after:to-transparent after:inset-0 after:absolute"
+      />
+      <Hero />
+      <MarqueeMember />
+      <Shade className='mt-10 border-y' />
+      <About />
+      <Shade className='border-y' />
+      <ReadyProjects />
+      <Shade className='border-y' />
+      <CallToActionJoin />
+      <Shade
+        corners={['bottom-left', 'bottom-right']}
+        cornerClass='border-foreground/75 border-b-0 h-12'
+        className="h-14 after:content-[''] after:w-full after:h-14 after:bg-gradient-to-t after:to-background after:from-transparent after:inset-0 after:absolute"
+      />
+    </>
+  );
+}
